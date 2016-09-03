06b9014126c4e8622e2e6c829317910a7650e641
Amitosh Gain <amitosh.seu@gmail.com>
2016-09-03 14:09:23 +0600

    add test class

---
+++ HowToProgramBook/test/Loop/test.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Loop;
+
+/**
+ *
+ * @author amitoshseu
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("This is test class for github");
+        System.out.println("Created by Amitosh Gain");
+        System.out.println("Bye");
+    }
+}

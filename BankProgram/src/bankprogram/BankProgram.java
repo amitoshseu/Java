3940b20f54a86a24f7eec7cc74a470164446dd07
amitoshseu <amitosh_cse_seu@yahoo.com>
2017-05-14 02:00:00 +0600

    Added some classes

---
+++ BankProgram/src/bankprogram/BankProgram.java
@@ -0,0 +1,20 @@
+/*
+    A customer opens an account in a bank, the customer must have an initial balance of 100$ 
+    while opening the account, the customer car withdraw, deposit, and check his balance any time
+    he wants, the bank does not charge any fees for the 1st withdrawal but for all subsequent withdrawals,
+    the bank charges some transaction fees. The bank also calculates a certain amount of interest o the
+    amount deposited by the customer as per its interest rate. Create an object oriented program to automate this problem.
+*/
+package bankprogram;
+
+/**
+ *
+ * @author amitoshseu
+ */
+public class BankProgram {
+
+    public static void main(String[] args) {
+     
+    }
+    
+}
